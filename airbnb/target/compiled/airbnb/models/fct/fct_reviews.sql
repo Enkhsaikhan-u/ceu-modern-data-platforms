4ae2aa8eaddfa6043bb622e150f4aa22c3dbc465
Enkhsaikhan-u <Enkhsaikhan680@gmail.com>
2026-01-22 15:18:32 +0100

    Add target

---
+++ airbnb/target/compiled/airbnb/models/fct/fct_reviews.sql
@@ -0,0 +1,25 @@
+
+
+WITH  __dbt__cte__src_reviews as (
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.raw.raw_reviews
+)
+SELECT
+    listing_id,
+    DATE AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM
+    raw_reviews
+), src_reviews AS (
+  SELECT *, current_timestamp() AS loaded_at FROM __dbt__cte__src_reviews
+)
+SELECT * 
+FROM src_reviews
+WHERE review_text is not null
+
+    AND review_date > (select max(review_date) from AIRBNB.DEV.fct_reviews)
