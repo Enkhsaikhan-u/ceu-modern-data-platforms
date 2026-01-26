1f585d71465d9f6b0c4604e2cb1d4f655d08798f
Enkhsaikhan-u <Enkhsaikhan680@gmail.com>
2026-01-26 17:08:19 +0100

    Day 2

--- airbnb/target/compiled/airbnb/models/fct/fct_reviews.sql
+++ airbnb/target/compiled/airbnb/models/fct/fct_reviews.sql
@@ -1,25 +1,27 @@
 
-
 WITH  __dbt__cte__src_reviews as (
 WITH raw_reviews AS (
-    SELECT
-        *
+    SELECT *
     FROM
         AIRBNB.raw.raw_reviews
 )
+
 SELECT
     listing_id,
-    DATE AS review_date,
+    date AS review_date,
     reviewer_name,
     comments AS review_text,
     sentiment AS review_sentiment
 FROM
     raw_reviews
 ), src_reviews AS (
-  SELECT *, current_timestamp() AS loaded_at FROM __dbt__cte__src_reviews
+    SELECT * FROM __dbt__cte__src_reviews
 )
-SELECT * 
-FROM src_reviews
-WHERE review_text is not null
 
-    AND review_date > (select max(review_date) from AIRBNB.DEV.fct_reviews)
+SELECT * FROM src_reviews
+WHERE
+    review_text IS NOT null
+
+    
+        AND review_date > (SELECT max(review_date) FROM AIRBNB.DEV.fct_reviews)
+    
--- airbnb/target/run/airbnb/models/fct/fct_reviews.sql
+++ airbnb/target/run/airbnb/models/fct/fct_reviews.sql
@@ -3,9 +3,9 @@
   begin;
     
 
-        insert into AIRBNB.DEV.fct_reviews ("LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT", "LOADED_AT")
+        insert into AIRBNB.DEV.fct_reviews ("LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT")
         (
-            select "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT", "LOADED_AT"
+            select "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT"
             from AIRBNB.DEV.fct_reviews__dbt_tmp
         );
     commit;
